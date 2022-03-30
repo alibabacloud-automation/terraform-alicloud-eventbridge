9412c3d0c06b131a19a0a0b7055b98f43110aa22
wb-ssc771735 <wb-ssc771735@alibaba-inc.com>
2022-03-30 18:53:33 +0800

    fix example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,38 +1,48 @@
 
 module "mns-queue" {
   source = "terraform-alicloud-modules/mns-queue/alicloud"
-  name   = "tf-test"
+  name   = local.queue_name
+}
+
+resource "random_uuid" "default" {}
+
+data "alicloud_account" "default" {}
+
+locals {
+  uuid            = substr("${replace(random_uuid.default.result, "-", "")}", 0, 6)
+  queue_name      = "tf-test${local.uuid}"
+  target_endpoint = "acs:mns:cn-beijing:${data.alicloud_account.default.id}:queues/${local.queue_name}"
 }
 
 module "example" {
   source                 = "../.."
   #alicloud_event_bridge_event_bus
   create_bus             = true
-  event_bus_name         = "tf-name"
+  event_bus_name         = "tf-busname${local.uuid}"
   bus_description        = var.bus_description
   #alicloud_event_bridge_event_source
-  create_source          = true
-  event_source_name      = "tftest"
+  create_source          = false
+  event_source_name      = "tfsource${local.uuid}"
   source_description     = var.source_description
   linked_external_source = var.linked_external_source
   external_source_type   = var.external_source_type
   external_source_config = {
-    QueueName = module.mns-queue.queue_name
+    QueueName = local.queue_name
   }
   #alicloud_event_bridge_rule
   create_rule            = true
-  rule_name              = "tf-name"
+  rule_name              = "tf-rulename${local.uuid}"
   rule_description       = var.rule_description
   rule_filter_pattern    = var.rule_filter_pattern
   rule_status            = var.rule_status
   target_id              = "tf-test"
-  target_endpoint        = var.target_endpoint
+  target_endpoint        = local.target_endpoint
   target_type            = var.target_type
   param_list = [
     {
     resource_key  = "queue"
     form          = "CONSTANT"
-    value         = "tf-test"
+    value         = local.queue_name
     template      = null
     },
     {
--- examples/complete/tfvars/01-update.tfvars
+++ examples/complete/tfvars/01-update.tfvars
@@ -5,6 +5,5 @@ external_source_type   = "MNS"
 rule_description       = "rule_description"
 rule_filter_pattern    = "{\"source\":[\"acs.ecs\"],\"type\":[\"ecs:Snapshot:CreateSnapshotCompleted\"]}"
 rule_status            = "DISABLE"
-target_endpoint        = "acs:mns:cn-beijing:123456xxxx:queues/tf-test"
 target_type            = "acs.mns.queue"
 
